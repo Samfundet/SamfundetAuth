277804a8a46034fb56aeeb866ba65a0737722ea1
alfiehub <alfiehub@users.noreply.github.com>
2018-02-06 19:26:53 +0100

    Another attempt at fixing the log silencer

--- app/models/member.rb
+++ app/models/member.rb
@@ -50,7 +50,7 @@ class Member < ActiveRecord::Base
   private
 
   def self.authenticate_production(member_id_or_email, password)
-    LoggerSilence.silence do # Prevents passwords from showing up in the logs.
+    Rails.logger.silence do # Prevents passwords from showing up in the logs.
       member_id = connection.select_value sanitize_sql([
                                                            "SELECT * FROM sett_lim_utvidet_medlemsinfo(?, ?)",
                                                            member_id_or_email.to_s,
